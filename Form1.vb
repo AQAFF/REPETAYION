248fd2cf8f6fb5b7d1cef28db965419ea0633604
TUF <TUF@LAPTOP-257ULQGP>
2022-04-22 01:30:27 +0800

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,29 @@
+﻿Public Class Form1
+    Private Sub Label1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim x, y As Integer
+        For x = 1 To 3
+            For y = 1 To 4
+                TextBox1.Text = TextBox1.Text & "#" & vbCr
+            Next y
+            TextBox1.Text = TextBox1.Text & "#" & vbCrLf
+
+        Next x
+
+        Dim z, a As Integer
+        For z = 1 To 3
+            For a = 1 To 4
+                TextBox2.Text = TextBox2.Text & "#" & vbCr
+            Next a
+            TextBox2.Text = TextBox2.Text & "#" & vbCrLf
+
+        Next z
+    End Sub
+End Class
